016a43f0bd63185e2fdedd325dc15e27a0244cda
Pritam <pritamnanda504@gmail.com>
2024-11-21 20:47:50 +0530

    Added main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+import 'theme/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MovieZZ',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: SplashScreen(),
+    );
+  }
+}
